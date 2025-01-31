a5b9086867db246bafd3c7a06a080d1326d96d15
javid shabin <javid.main.act@gmail.com>
2025-02-01 00:33:21 +0530

    make all configration in main index.js file like add express.json , cors, cookie parser, and helmet this . and connect database befor running server its more better and effienct way

---
+++ README.md
@@ -0,0 +1,26 @@
+# Project Title
+
+**CareofNest**
+
+## Description
+
+**CareofNest** is a virtual healthcare platform designed to deliver reliable and accessible medical services. It is particularly beneficial for individuals with specific needs, such as pregnant women, elderly patients, and people with disabilities.
+
+## Dependencies
+
+Install the following packages:
+
+- `express`  
+- `dotenv`  
+- `cors`  
+- `mongoose`  
+- `bcrypt`  
+- `cookie-parser`  
+- `helmet`  
+- `jsonwebtoken`  
+- `nodemailer`
+
+**Setup Instructions:**
+
+- Add `"type": "module"` in your `package.json`
+- Install and configure `mongoose` to connect the server with the MongoDB database
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { connectDb } from "./configs/database.js";
+
 const PORT = 4000;
+const app = express();
 
+// Middleware
+app.use(helmet());
 app.use(express.json());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+app.use(cookieParser()); // Cookie parser
 
-app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
-})
+// Start the server
+connectDb()
+  .then(() => {
+    console.log("Connected to MongoDB"); // First connect the databse
+    app.listen(PORT, () => {
+      // Then start the server
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
