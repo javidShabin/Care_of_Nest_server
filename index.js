bbf793491ddc4194a63d2c4acdaab22e2a3bdfc1
javid shabin <javid.main.act@gmail.com>
2025-02-01 00:25:05 +0530

    Create basic express server for the health caring website, and install some dependencies. Create the folder structer and add the .env .gitignore files

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
