83fe3639ca3e0976c6c3523b3eff62990069070a
javid shabin <javid.main.act@gmail.com>
2025-02-05 14:53:11 +0530

    Create the middleware for patient authenctication

---
+++ src/middlewares/patient_auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/createError.js";
+
+export const patientAuth = (req, res, next) => {
+  try {
+    // Extract the token from the cookies
+    const { patientToken } = req.cookies;
+    // Check if the token is present
+    if (!patientToken) {
+      return next(createError(401, "Unauthorized access"));
+    }
+    // Verify the token
+    const verifyToken = jwt.verify(patientToken, process.env.JWT_SECRET_KEY);
+    // Check if the token is valid
+    if (!verifyToken) {
+      return next(createError(401, "Unauthorized access"));
+    }
+    // If the token is valid, proceed to the next middleware or route handler
+    req.user = verifyToken;
+    next();
+  } catch (error) {
+    console.error("Error in patientAuth middleware:", error);
+    return next(error)
+  }
+};
