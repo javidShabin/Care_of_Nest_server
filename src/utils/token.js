b8b0fb3050601ce10b8c72566509bf0f7e0f81b7
javid shabin <javid.main.act@gmail.com>
2025-02-15 01:17:12 +0530

    create admin auth empty file for creating auth middleware for admin, and make functin inside token.js for generatinJWT admin token

--- src/utils/token.js
+++ src/utils/token.js
@@ -39,3 +39,23 @@ export const generateDoctorToken = ({ _id, email, role }) => {
     return null;
   }
 };
+
+// Generate a JWT token for a doctor
+export const generateAdminToken = ({ _id, email, role }) => {
+  try {
+    // Create a token with doctor's ID and email
+    const token = jwt.sign(
+      {
+        id: _id,
+        email: email,
+        role: role,
+      },
+      process.env.JWT_SECRET_KEY, // replace with your secret key
+      { expiresIn: "5h" } // Set an expiration time for the token
+    );
+    return token;
+  } catch (error) {
+    console.error("Error generating token:", error);
+    return null;
+  }
+};
