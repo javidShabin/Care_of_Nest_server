fe43be2bbc91f1cdb8a73157383941795b54bff4
javid shabin <javid.main.act@gmail.com>
2025-02-18 00:47:47 +0530

    Set and test the doctor own profile get function endpoint

--- src/modules/doctors/doctor_router.js
+++ src/modules/doctors/doctor_router.js
@@ -5,6 +5,7 @@ import {
     fetchAdminVerifiedDoctors,
     fetchAllDoctors,
   getDoctorProfileById,
+  getLoggedInDoctorProfile,
   loginDoctor,
   registerDoctor,
   verifyDoctorOtpAndCreateAccount,
@@ -18,5 +19,6 @@ doctorRouter.post("/login_doctor", loginDoctor);
 doctorRouter.get("/get_doctors_list", fetchAllDoctors)
 doctorRouter.get("/verifyed_doctors_list", fetchAdminVerifiedDoctors)
 doctorRouter.get("/doctor_profile/:_id", getDoctorProfileById)
+doctorRouter.get("/doctor_loged_profile", doctorAuth, getLoggedInDoctorProfile)
 
 export default doctorRouter;
