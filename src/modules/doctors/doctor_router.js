5d76973209d138f45490d1b62a8df6d9e57ad7f9
javid shabin <javid.main.act@gmail.com>
2025-02-25 00:35:31 +0530

    Set the router and complete testing of the endpoint login doctor

--- src/modules/doctors/doctor_router.js
+++ src/modules/doctors/doctor_router.js
@@ -1,11 +1,16 @@
-import express, {Router} from "express"
-import {doctorAuth} from "../../middlewares/doctor_auth.js"
+import express, { Router } from "express";
+import { doctorAuth } from "../../middlewares/doctor_auth.js";
 import { upload } from "../../middlewares/multer.js";
-import { registerDoctor, verifyDoctorOtpAndCreateAccount } from "./doctor_controller.js";
+import {
+  loginDoctor,
+  registerDoctor,
+  verifyDoctorOtpAndCreateAccount,
+} from "./doctor_controller.js";
 
 const doctorRouter = express.Router();
 
-doctorRouter.post("/register_doctor", registerDoctor)
-doctorRouter.post("/verify_otp", verifyDoctorOtpAndCreateAccount)
+doctorRouter.post("/register_doctor", registerDoctor);
+doctorRouter.post("/verify_otp", verifyDoctorOtpAndCreateAccount);
+doctorRouter.post("/login_doctor", loginDoctor);
 
-export default doctorRouter
+export default doctorRouter;
