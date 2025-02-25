d9b3b6da2fb62a636dd53e279237a9982b118ceb
javid shabin <javid.main.act@gmail.com>
2025-02-25 21:19:02 +0530

    add the doctor router in api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import express from "express"
 import patientRouter from "./modules/patients/patient_router.js";
+import doctorRouter from "./modules/doctors/doctor_router.js";
 const apiRouter = express.Router()
 
 apiRouter.use("/patient", patientRouter)
+apiRouter.use("/doctor", doctorRouter)
 
 export default apiRouter;
