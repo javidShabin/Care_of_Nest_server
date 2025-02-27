370b89bf0b54609a2db5281f19582bdce0da3688
javid shabin <javid.main.act@gmail.com>
2025-02-28 00:41:37 +0530

    Set the router and end point for verify docntor list

--- src/modules/doctors/doctor_router.js
+++ src/modules/doctors/doctor_router.js
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import { doctorAuth } from "../../middlewares/doctor_auth.js";
 import { upload } from "../../middlewares/multer.js";
 import {
+    fetchAdminVerifiedDoctors,
     fetchAllDoctors,
   loginDoctor,
   registerDoctor,
@@ -14,6 +15,6 @@ doctorRouter.post("/register_doctor", registerDoctor);
 doctorRouter.post("/verify_otp", verifyDoctorOtpAndCreateAccount);
 doctorRouter.post("/login_doctor", loginDoctor);
 doctorRouter.get("/get_doctors_list", fetchAllDoctors)
-
+doctorRouter.get("/verifyed_doctors_list", fetchAdminVerifiedDoctors)
 
 export default doctorRouter;
