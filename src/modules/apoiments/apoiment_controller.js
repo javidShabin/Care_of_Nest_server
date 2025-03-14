14f1e3ccc331cb22c743ef8425d772aef036a498
javid shabin <javid.main.act@gmail.com>
2025-03-14 19:49:18 +0530

    remove the book apoiment controller and router so add insted of that get available slots a specific doctor

--- src/modules/apoiments/apoiment_controller.js
+++ src/modules/apoiments/apoiment_controller.js
@@ -1,9 +1,5 @@
-import Appointment from '../apoiments/apoiment_model.js';
-import Doctor from '../doctors/temp_doctor_model.js';
+import Appointment from "../apoiments/apoiment_model.js";
+import Doctor from "../doctors/doctor_model.js";
 
-export const bookAppointment = async (req, res, next) => {
-    const { doctorId, date, timeSlot, reason } = req.body;
-    const userId = req.patient.id
-    
+// Get the doctor availability
 
-}
--- src/modules/apoiments/apoiment_router.js
+++ src/modules/apoiments/apoiment_router.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import { bookAppointment } from './apoiment_controller.js';
 import { patientAuth } from '../../middlewares/patient_auth.js';
 const apoimentRouter = express.Router();
 
-apoimentRouter.post("/book_apoiment", patientAuth, bookAppointment)
 
 export default apoimentRouter
