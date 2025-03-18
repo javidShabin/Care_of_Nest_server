8f930b7753985758d6dcddb22866f20870a6eb82
javid shabin <javid.main.act@gmail.com>
2025-03-18 12:21:22 +0530

    Create a apoiments folder inside the module folder and add apoiment model.js , config the file of adding needed fields

---
+++ src/modules/apoiments/apoiment_model.js
@@ -0,0 +1,34 @@
+// models/Appointment.js
+import mongoose from 'mongoose';
+
+const appointmentSchema = new mongoose.Schema({
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Patient',
+    required: true,
+  },
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Doctor',
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  timeSlot: {
+    type: String,
+    required: true,
+  },
+  reason: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'cancelled'],
+    default: 'pending',
+  },
+});
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+export default Appointment;
