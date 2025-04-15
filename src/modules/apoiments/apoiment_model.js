f77bb70af8e02db62042275869d3168217279793
javid shabin <javid.main.act@gmail.com>
2025-04-15 05:46:52 +0530

    Add extra one field in apoiment schema for store th apoiment day

--- src/modules/apoiments/apoiment_model.js
+++ src/modules/apoiments/apoiment_model.js
@@ -1,36 +1,41 @@
 // models/Appointment.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const appointmentSchema = new mongoose.Schema({
-  patientId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Patient',
-    required: true,
+const appointmentSchema = new mongoose.Schema(
+  {
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    day: {
+      type: String,
+      required: true,
+    },
+    timeSlot: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    reason: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "pending",
+    },
   },
-  doctorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Doctor',
-    required: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  timeSlot: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  reason: {
-    type: String,
-  },
-  status: {
-    type: String,
-    enum: ['pending', 'confirmed', 'cancelled'],
-    default: 'pending',
-  },
-},
-{timestamps: true}
+  { timestamps: true }
 );
 
-const Appointment = mongoose.model('Appointment', appointmentSchema);
+const Appointment = mongoose.model("Appointment", appointmentSchema);
 export default Appointment;
