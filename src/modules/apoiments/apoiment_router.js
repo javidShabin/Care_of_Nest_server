02e60a61b4a5e33e0e70b8f5066570888bf31bbe
javid shabin <javid.main.act@gmail.com>
2025-04-19 06:09:48 +0530

    Set router for the get all apoiments for doctor and add the authentication

--- src/modules/apoiments/apoiment_router.js
+++ src/modules/apoiments/apoiment_router.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { patientAuth } from '../../middlewares/patient_auth.js';
-import { bookApoiment, getDoctorAvailability, getPatientAppointments } from './apoiment_controller.js';
+import { bookApoiment, getDoctorAppointments, getDoctorAvailability, getPatientAppointments } from './apoiment_controller.js';
+import { doctorAuth } from '../../middlewares/doctor_auth.js';
 const apoimentRouter = express.Router();
 
 apoimentRouter.get("/available_slot/:doctorId", getDoctorAvailability )
 apoimentRouter.post("/book_slot", patientAuth, bookApoiment)
 apoimentRouter.get("/get_apoiment_patient", patientAuth, getPatientAppointments)
+apoimentRouter.get("/get_apoiment_doctor", doctorAuth, getDoctorAppointments)
 
 export default apoimentRouter
