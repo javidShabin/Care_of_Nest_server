1d6b452141de5aedff1853db067b9bfb1dece120
javid shabin <javid.main.act@gmail.com>
2025-04-21 06:27:21 +0530

    set the route for cancel appoinment

--- src/modules/apoiments/apoiment_router.js
+++ src/modules/apoiments/apoiment_router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { patientAuth } from '../../middlewares/patient_auth.js';
-import { bookApoiment, getDoctorAppointments, getDoctorAvailability, getPatientAppointments } from './apoiment_controller.js';
+import { bookApoiment, cancelAppointment, getDoctorAppointments, getDoctorAvailability, getPatientAppointments } from './apoiment_controller.js';
 import { doctorAuth } from '../../middlewares/doctor_auth.js';
 const apoimentRouter = express.Router();
 
@@ -8,5 +8,6 @@ apoimentRouter.get("/available_slot/:doctorId", getDoctorAvailability )
 apoimentRouter.post("/book_slot", patientAuth, bookApoiment)
 apoimentRouter.get("/get_apoiment_patient", patientAuth, getPatientAppointments)
 apoimentRouter.get("/get_apoiment_doctor", doctorAuth, getDoctorAppointments)
+apoimentRouter.patch("/cancel_appoinment/:id", patientAuth, cancelAppointment)
 
 export default apoimentRouter
