a4ff65db2dcb3a446c7812cca46b12a112b664ec
javid shabin <javid.main.act@gmail.com>
2025-05-11 20:46:35 +0530

    Create function for a specific doctor's availablities

--- src/modules/apoiments/apoiment_controller.js
+++ src/modules/apoiments/apoiment_controller.js
@@ -1,5 +1,22 @@
+import { createError } from "../../utils/createError.js";
 import Appointment from "../apoiments/apoiment_model.js";
 import Doctor from "../doctors/doctor_model.js";
 
 // Get the doctor availability
-
+export const getDoctorAvailability = async (req, res, next) => {
+  // Get docrot id from request params
+  const { doctorId } = req.params;
+  try {
+    // Find the doctor by id
+    const doctor = await Doctor.findById(doctorId);
+    if (!doctor) {
+      return next(createError(404, "Doctor not found"));
+    }
+    res.json({
+      doctorId,
+      availability: doctor.availability,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- src/modules/apoiments/apoiment_router.js
+++ src/modules/apoiments/apoiment_router.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { patientAuth } from '../../middlewares/patient_auth.js';
+import { getDoctorAvailability } from './apoiment_controller.js';
 const apoimentRouter = express.Router();
 
+apoimentRouter.get("/available_slot/:doctorId", getDoctorAvailability )
 
 export default apoimentRouter
